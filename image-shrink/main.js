f800efeff06e7566bd9484a8562eda0d96e77d53
prathabanKavin <kavinprathaban025@gmail.com>
2021-01-06 00:17:12 +0530

    added enviroment variables and nodemon added

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,13 +1,21 @@
 const { app, BrowserWindow } = require('electron')
 
+//Set environment
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isWindows = process.platform === 'win32' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
 let mainWindow
 
 function createMainWindow(){
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title:'ImageShrink',
         width:500,
         height:600,
-        icon: './assets/icons/Icon_256x256.png'
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: isDev ? true : false,
     })
 
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
@@ -15,3 +23,15 @@ function createMainWindow(){
 }
 
 app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+    if(!isMac){
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createMainWindow()
+    }
+})
