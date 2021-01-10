9d5db47520468e9d2dfbeff07261bd7201ae9c63
prathabanKavin <kavinprathaban025@gmail.com>
2021-01-10 21:10:28 +0530

    added about page and aboutMenu

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -8,6 +8,7 @@ const isWindows = process.platform === 'win32' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let aboutWindow
 
 function createMainWindow(){
     mainWindow = new BrowserWindow({
@@ -23,23 +24,59 @@ function createMainWindow(){
     //mainWindow.loadFile('./app/index.html')
 }
 
+function createAboutWindow(){
+    aboutWindow = new BrowserWindow({
+        title:'About ImageShrink',
+        width:300,
+        height:300,
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: false,
+        backgroundColor: 'white'
+    })
+
+    aboutWindow.loadURL(`file://${__dirname}/app/about.html`)
+    //aboutWindow.loadFile('./app/about.html')
+}
+
 app.on('ready', () => {
     createMainWindow()
 
     const mainMenu = Menu.buildFromTemplate(menu)
     Menu.setApplicationMenu(mainMenu)
 
-    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+    //no need of this shortcuts since developermenu is enabled
+    //globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+    //globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
 
     mainWindow.on('ready', () => mainWindow = null)
 })
 
 const menu = [
-    ...(isMac ? [{ role: 'appMenu' }] : []),
+    ...(isMac ? [
+        { 
+            label: app.name,
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ] : []),
     {
         role: 'fileMenu'
     },
+    ...(!isMac ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ] : []),
     ...(isDev ? [ 
         {
             label: 'Developer',
