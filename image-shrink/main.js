0bbafc46a9c442cc1b53ee31fd43eb9bd6e7fbec
prathabanKavin <kavinprathaban025@gmail.com>
2021-01-10 14:02:09 +0530

    added menu shortcuts and globalShortcuts

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
 
 //Set environment
 process.env.NODE_ENV = 'development'
@@ -16,13 +16,39 @@ function createMainWindow(){
         height:600,
         icon: './assets/icons/Icon_256x256.png',
         resizable: isDev ? true : false,
+        backgroundColor: 'white'
     })
 
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     //mainWindow.loadFile('./app/index.html')
 }
 
-app.on('ready', createMainWindow)
+app.on('ready', () => {
+    createMainWindow()
+
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
+    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+
+    mainWindow.on('ready', () => mainWindow = null)
+})
+
+const menu = [
+    ...(isMac ? [{ role: 'appMenu' }] : []),
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                //accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+                accelerator: 'CmdOrCtrl+W',
+                click: () => app.quit()
+            }
+        ]
+    }
+]
 
 app.on('window-all-closed', () => {
     if(!isMac){
