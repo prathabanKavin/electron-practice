170f806d8827f37c7601322ecb1283f6adcfd5ba
prathabanKavin <kavinprathaban025@gmail.com>
2021-01-12 00:17:53 +0530

    added imagemin package and implemented shrinking

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, globalShortcut, ipcMain } = require('electron')
+const path = require('path')
+const os = require('os')
+const { app, BrowserWindow, Menu, globalShortcut, ipcMain, shell } = require('electron')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
 
 //Set environment
 process.env.NODE_ENV = 'development'
@@ -98,8 +104,30 @@ const menu = [
 ]
 
 ipcMain.on('image:minimize', (e, options) => {
-    console.log(options)
+    options.dest = path.join(os.homedir(), 'imageshrink')
+    shrinkImage(options)
 })
+
+async function shrinkImage({ imgPath, quality, dest }){
+    try {
+        const pngQuality = quality/100
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugins: [
+                imageminMozjpeg({ quality }),
+                imageminPngquant({ 
+                    quality : [pngQuality, pngQuality] 
+                })
+            ]
+        })
+
+        console.log(files)
+
+        shell.openPath(dest)
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 app.on('window-all-closed', () => {
     if(!isMac){
